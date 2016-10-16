def598cf26696acd75211a8c651f3c0a7209a1ec
Nathan Walker <walkerrunpdx@gmail.com>
2016-10-15 17:27:50 -0700

    setup

---
+++ nativescript/references.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" />
+/// <reference path="../node_modules/@types/jasmine/index.d.ts" />
+/// <reference path="../node_modules/@types/lodash/index.d.ts" />
+/// <reference path="../node_modules/@types/zone.js/index.d.ts" />
