ffb32eeafa6d0f46120b0e2fd706d19dd20b1f7f
George Edwards <g.edwards@gas-sense.co.uk>
2016-11-23 13:57:06 +0000

    initial grid

--- src/client/app/components/home/home.component.ts
+++ src/client/app/components/home/home.component.ts
@@ -19,9 +19,10 @@ import { AUTH_ACTIONS, IAuthState } from '../../frameworks/progress/services/aut
 export class HomeComponent implements OnInit, OnDestroy {
   public current: IUser;
   private _sub: Subscription;
+  plugins: Array<plugin>;
 
   constructor(private store: Store<any>) {
-
+    this.plugins = plugins;
   }
 
   public login() {
@@ -42,3 +43,69 @@ export class HomeComponent implements OnInit, OnDestroy {
     if (this._sub) this._sub.unsubscribe();
   }
 }
+declare class plugin {
+  title: string;
+  author: string;
+  stars: number;
+  description: string;
+  ios: boolean;
+  android: boolean;
+  repo: string;
+}
+
+var plugins: Array<plugin> = [
+  {
+    title: 'SQLLite',
+    author: 'nathanael',
+    stars: 5,
+    description: 'A sqlite Nativescript module for ios and android',
+    ios: true,
+    android: true,
+    repo: 'https://github.com/nathanael/sqlite'
+  },
+  {
+    title: 'Shimer',
+    author: 'walkerrunpdx',
+    stars: 5,
+    description: 'Facebook shimer effect plugin',
+    ios: true,
+    android: true,
+    repo: 'https://github.com/walkerrunpdx/shimer'
+  },
+  {
+    title: 'Orientation',
+    author: 'nathanael',
+    stars: 5,
+    description: 'A sqlite Nativescript module for ios and android',
+    ios: true,
+    android: true,
+    repo: 'https://github.com/nathanael/sqlite'
+  },
+  {
+    title: 'CardView',
+    author: 'walkerrunpdx',
+    stars: 5,
+    description: 'Facebook shimer effect plugin',
+    ios: true,
+    android: true,
+    repo: 'https://github.com/walkerrunpdx/shimer'
+  },
+  {
+    title: 'SQLLite',
+    author: 'nathanael',
+    stars: 5,
+    description: 'A sqlite Nativescript module for ios and android',
+    ios: true,
+    android: true,
+    repo: 'https://github.com/nathanael/sqlite'
+  },
+  {
+    title: 'Shimer',
+    author: 'walkerrunpdx',
+    stars: 5,
+    description: 'Facebook shimer effect plugin',
+    ios: true,
+    android: true,
+    repo: 'https://github.com/walkerrunpdx/shimer'
+  }
+]
