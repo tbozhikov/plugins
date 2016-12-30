c2ff7b4d3307dea1161db6f678f8de12dbe664a7
George Edwards <g.edwards@gas-sense.co.uk>
2016-12-30 16:07:11 +0000

    feat: fix fonts

--- src/client/app/pipes/url-shorten.pipe.ts
+++ src/client/app/pipes/url-shorten.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'urlShorten'})
 export class urlShortenPipe implements PipeTransform {
   transform(value: string): string {
-    let res = value.substring(0,29);
+    let res = value.substring(0, 45);
     return res + '...';
   }
 }
