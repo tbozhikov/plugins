9f0538a34ec846953dac98018c7687f8033f4b85
Nathan Walker <walkerrunpdx@gmail.com>
2017-01-15 15:21:07 -0800

    prep script

--- prep.js
+++ prep.js
@@ -1,7 +1,7 @@
 var fs = require('fs-extra');
 
 try {
-  fs.copySync('/dist/prod', '/server/dist');
+  fs.copySync('dist/prod', 'server/dist');
   console.log("client build copied to server, ready for deploy!");
 } catch (err) {
   console.error(err);
