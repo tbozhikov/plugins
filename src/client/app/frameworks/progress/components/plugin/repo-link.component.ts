334be1087f07ae19adc8c1ecbcdf15d0fed3d7be
George Edwards <g.edwards@gas-sense.co.uk>
2017-01-16 22:11:24 +0000

    resp plugins boxes

--- src/client/app/frameworks/progress/components/plugin/repo-link.component.ts
+++ src/client/app/frameworks/progress/components/plugin/repo-link.component.ts
@@ -3,7 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'repo-link',
-  templateUrl: 'repo-link.component.html'
+  templateUrl: 'repo-link.component.html',
+  styles: [':host { position: absolute; width: calc(100% - 55px); }']
+
 })
 export class RepoLinkComponent {
   @Input() plugin;
